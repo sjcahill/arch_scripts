2069932f670fcffb60b2ebeaf982fabb63db488f
sjcahill <sjoscahill@gmail.com>
2021-12-20 10:49:31 -0500

    Minor cleanup of install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -8,7 +8,7 @@ blue='\033[0;34m'
 green='\033[0;32m'
 nc='\033[0m'
 
-# Remove any possible directories that contain nvim date
+# Remove any possible directories that contain nvim data
 
 if [[ -d /usr/local/share/nvim ]]; then
 	$(sudo rm -rf nvim)
@@ -42,18 +42,6 @@ function install_neovim() {
 	sudo make install
 }
 	
-
-: '
-$(cd "${HOME}/Downloads/neovim" &&
-	git checkout stable
-	rm -rf build/ &&
-	echo -e "${blue}Modifying the install directory with cmake flags${nc}" &&
-	make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${HOME}/neovim" &&
-	echo -e "${blue}Installing neovim with cmake${nc}" &&
-	sudo make install
-)
-'
-
 install_neovim
 
 echo -e "${green}Successfully built neovim from source. Should be located at: ${HOME}/neovim ${nc}"
